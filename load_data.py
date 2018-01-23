83026dcc9c29e7298360f1fe08a79a9b578da985
wyzdwdz <wyzdwdz@gmail.com>
2018-01-23 10:52:00 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 
+#生成一组{年份:文件路径}的字典year_str
+#和一组{年份:[多个xls的Sheet名组成的list]}的字典month_str
 years = list(range(2003, 2009))
 months = list(range(1, 13))
 year_str = {}
@@ -15,6 +17,7 @@ for year in years:
 for i in range(6):
     month_str[2008].pop()
 
+#将所有文件和所有文件里的Sheets连接到一起组成DataFrame
 data = DataFrame()
 for year in year_str:
     with pd.ExcelFile(year_str[year]) as xls:
