958f9c4a67a25319a3ea0ac649833583c5d60a6d
wyzdwdz <wyzdwdz@gmail.com>
2018-01-24 10:02:47 +0800

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -4,4 +4,4 @@ from pandas import DataFrame, Series
 import re
 import datetime as dt
 
-data = pd.read_csv('', index_col=['类型', 'ID', '日期'])
+data = pd.read_csv('C://Users//wyzdw//Desktop//data2.csv', index_col=['类型', 'ID', '日期'])
