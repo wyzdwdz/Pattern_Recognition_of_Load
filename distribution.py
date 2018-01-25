d78b1af0d1e86b90dd421c29b2a70394d482fdbe
wyzdwdz <wyzdwdz@gmail.com>
2018-01-25 12:52:02 +0800

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -10,3 +10,11 @@ data = data.drop_duplicates()
 np.size(np.where(np.isnan(data)))
 data.index.is_lexsorted()
 data.loc(axis=0)[:, 'H00011']
+
+b_list = []
+for a, b, c in list(data.index.values):
+    if b in b_list:
+        continue
+    else:
+        b_list.append(b)
+        
