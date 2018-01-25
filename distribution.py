887f57949649a0b87d55684e254011f40bcf60cd
wyzdwdz <wyzdwdz@gmail.com>
2018-01-25 10:52:40 +0800

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -3,6 +3,7 @@ import numpy as np
 from pandas import DataFrame, Series
 import re
 import datetime as dt
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('C://Users//wyzdw//Desktop//data2.csv', index_col=['类型', 'ID', '日期'])
 data = data.drop_duplicates()
