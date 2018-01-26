9c3c1514073e092a2bb1f491bde2e83282bffed2
wyzdwdz <wyzdwdz@gmail.com>
2018-01-26 20:55:31 +0800

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('C://Users//wyzdw//Desktop//data2.csv', index_col=['类型', 'ID', '日期'])
 data = data.drop_duplicates()
 np.size(np.where(np.isnan(data)))
+data = data.sort_index()
 data.index.is_lexsorted()
 data.loc(axis=0)[:, 'H00011']
 
